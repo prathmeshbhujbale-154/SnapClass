435da3306169b5305125bc281640241f52be932e
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-07-29 21:27:28 +0530

    setup teacher screen

--- src/components/footer.py
+++ src/components/footer.py
@@ -8,3 +8,12 @@ def footer_home():
             <p style="font-weight:bold; color:white;"> Created by Prathmesh B. 🕸 </p>
         </div>
     """, unsafe_allow_html=True)
+
+def footer_dashboard():
+
+    logo_url= "https://i.ibb.co/YTYGn5qV/logo.png"
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center;">
+            <p style="font-weight:bold; color:black;"> Created by Prathmesh B. 🕸 </p>
+        </div>
+    """, unsafe_allow_html=True)
--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -1,5 +1,79 @@
 import streamlit as st
+from src.ui.base_layout import style_background_dashboard, style_base_layout
+from src.components.header import header_dashboard
+from src.components.footer import footer_dashboard
 
 
 def teacher_screen():
-    st.header("teacher screen")
+
+    style_background_dashboard()
+    style_base_layout()
+
+    
+    if "teacher_login_type" not in st.session_state or st.session_state.teacher_login_type=="login":
+        teacher_screen_login()
+    elif st.session_state.teacher_login_type == "register":
+        teacher_screen_register()
+
+def teacher_screen_login():
+    c1,c2 = st.columns(2, vertical_alignment='center', gap='xxlarge')
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button("go back to Home", type="secondary", key="loginbackbtn", shortcut="control+backspace"):
+            st.session_state["login_type"] = None
+            st.rerun()
+
+
+    st.header("Login using password", text_alignment="center")
+    st.space()
+    st.space()
+    teacher_username = st.text_input("Enter username", placeholder="username")
+
+    teacher_pass = st.text_input("Enter password", type="password", placeholder="password" )
+
+    st.divider()
+
+    btc1, btc2 = st.columns(2)
+    with btc1:
+        st.button("Login",icon=":material/passkey:", type="secondary", key="loginbtn", shortcut="control+enter",width="stretch")
+    with btc2:
+        if st.button("Register",icon=":material/passkey:", type="primary", key="registerbtn",width="stretch"):
+            st.session_state.teacher_login_type = "register"
+
+    footer_dashboard()
+
+
+
+def teacher_screen_register():
+    c1,c2 = st.columns(2, vertical_alignment='center', gap='xxlarge')
+    with c1:
+        header_dashboard()
+    with c2:
+        if st.button("go back to Home", type="secondary", key="loginbackbtn", shortcut="control+backspace"):
+            st.session_state["login_type"] = None
+            st.rerun()
+
+    st.header("Register your profile", text_alignment="center")
+
+    st.space()
+    st.space()
+    teacher_username = st.text_input("Enter username", placeholder="username")
+
+    teacher_name = st.text_input("Enter name", placeholder="name")
+
+    teacher_pass = st.text_input("Enter password", type="password", placeholder="password" )
+
+    teacher_confirm_pass = st.text_input("confirm password",type="password", placeholder="confirm your password")
+    st.divider()
+
+    btc1, btc2 = st.columns(2)
+    with btc1:
+        st.button("Register",icon=":material/passkey:", type="primary", key="registerbtn",width="stretch")
+
+    with btc2:
+        if st.button("Login Instead",icon=":material/passkey:", type="secondary", key="loginbtn", shortcut="control+enter",width="stretch"):
+            st.session_state.teacher_login_type = 'login'
+
+    footer_dashboard()
+
