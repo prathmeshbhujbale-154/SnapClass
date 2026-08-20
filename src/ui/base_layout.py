192e621df31165aec139a7f62d5c6fb7028a6a53
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-08-21 00:16:04 +0530

    final touches

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -36,9 +36,9 @@ def style_base_layout():
         @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&family=Outfit:wght@100..900&display=swap');
 
             /* hide top bar of streamlit*/
-          #  #MainMenu, footer, header {
-           #     visibility: hidden;
-           # }
+            #MainMenu, footer, header {
+                visibility: hidden;
+            }
             
             .block-container {
                 padding-top: 1.5rem !important
