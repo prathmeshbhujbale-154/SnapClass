c0cfdcca77dbc4acabe232b5d2a264ee15f2d811
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-08-21 11:21:00 +0530

    some  minor changes about the color of i'm student and i'm teacher

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -4,15 +4,15 @@ def style_background_home():
     
     st.markdown("""
         <style>
-                .stApp {
-                background: #5865F2 !important;
-                }
+            .stApp {
+            background: #5865F2 !important;
+            }
 
-                .stApp div[data-testid="stColumn"] {
-                background-color:#E0E3FF !important;
-                padding:2.5rem !important;
-                border-radius: 5rem !important;
-                }
+            .stApp div[data-testid="stColumn"] {
+            background-color:#E0E3FF !important;
+            padding:2.5rem !important;
+            border-radius: 5rem !important;
+            }
         </style>
                 """
             , unsafe_allow_html=True)
