8ab75da2d990a2fdc7ab6f9c1651d095020a1b2d
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-08-22 11:49:46 +0530

    added !important to it

--- src/screens/home_screen.py
+++ src/screens/home_screen.py
@@ -12,7 +12,7 @@ def home_screen():
     col1, col2 = st.columns(2, gap="large")
     with col1:
         st.markdown(
-            "<h2 style='color: black;'>I'm Student</h2>",
+            "<h2 style='color: black !important;'>I'm Student</h2>",
             unsafe_allow_html=True
         )
         st.image("https://i.ibb.co/844D9Lrt/mascot-student.png", width=120)
