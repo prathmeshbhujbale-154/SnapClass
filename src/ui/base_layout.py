d79d1642dc0f2d0bc000babb21351c9ff01d08fd
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-08-22 11:52:15 +0530

    hard change color from base layout

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -70,10 +70,12 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
+                color: black !important;
             }
                 
             h3, h4, p {
-                font-family: 'Outfit', sans-serif;    
+                font-family: 'Outfit', sans-serif;
+                color: black !important;    
             }
                 
 
