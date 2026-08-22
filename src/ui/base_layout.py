fd11c4a6bc3691f425532ce2ce2b249bddfc680c
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-08-22 12:46:21 +0530

    try change

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -70,9 +70,15 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                color: black !important;
             }
-                
+            .main-h2{
+                font-family: 'Climate Crisis', sans-serif !important;
+                font-size: 2rem !important;
+                line-height:0.9 !important;
+                margin-bottom:0rem !important;
+                color: black !important
+
+            }  
             h3, h4, p {
                 font-family: 'Outfit', sans-serif;   
             }
