61ad51c0c85755e65fb10657ac2f0ac9e8baf954
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-08-22 12:21:33 +0530

    h2 black

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -70,7 +70,7 @@ def style_base_layout():
                 font-size: 2rem !important;
                 line-height:0.9 !important;
                 margin-bottom:0rem !important;
-                color: black;
+                color: black !important;
             }
                 
             h3, h4, p {
