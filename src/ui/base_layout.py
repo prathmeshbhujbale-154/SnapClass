080934e52836c3c8a7098ce3331c7bfe2f15fe2b
Prathmesh Bhujbale <bhujbaleprathmesh@gmail.com>
2026-08-22 10:45:31 +0530

    back to original ui

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -60,7 +60,7 @@ def style_base_layout():
             h1 {
                 font-family: 'Climate Crisis', sans-serif !important;
                 font-size: 3.5rem !important;
-                line-height:1.1 !important;
+                line-height:1.1 1important;
                 margin-bottom:0rem !important;
             }
                 
@@ -77,41 +77,6 @@ def style_base_layout():
             }
                 
 
-            div[data-testid="stTextInput"] div[data-baseweb="input"] {
-                background-color: #FFFFFF !important;
-                border-radius: 0.7rem !important;
-                border: 1px solid #CCCCCC !important;
-            }
-
-            div[data-testid="stTextInput"] div[data-baseweb="base-input"] {
-                background-color: #FFFFFF !important;
-            }
-
-            div[data-testid="stTextInput"] input {
-                background-color: #FFFFFF !important;
-                color: #000000 !important;
-                -webkit-text-fill-color: #000000 !important;
-            }
-
-            /* Placeholder */
-            div[data-testid="stTextInput"] input::placeholder {
-                color: #777777 !important;
-                opacity: 1 !important;
-            }
-
-            /* Password eye button */
-            div[data-testid="stTextInput"] button {
-                background-color: #5865F2 !important;
-                color: #FFFFFF !important;
-            }
-
-            button {
-                border-radius: 1.5rem !important;
-                background-color: #5865F2 !important;
-                color: white !important;
-                padding: 10px 20px !important;
-                border: none !important;
-            }
             button{
                 border-radius: 1.5rem !important;
                 background-color: #5865F2 !important;
